5b403838e23ee0bcbbba54638f089f1b2af4af17
Apuroopa <65236102+Apuroopa25@users.noreply.github.com>
2022-12-06 12:54:53 +0530

    Update print.py

--- print.py
+++ print.py
@@ -1,2 +1,10 @@
 # print function uses to display your output on the console.
 print("Hello World!")
+
+# You have to use a "Double Quotes" or 'Single Quote' inside a print function to print a string type output.
+print('Hello World')
+
+# You can't use double quotes inside double quotes "" and single quote insde single quote ''.
+# But you can use double quote inside single quote and vice versa.
+
+print(" Hello World! 'My name is Apuroopa. Happy to survive!!!'")
